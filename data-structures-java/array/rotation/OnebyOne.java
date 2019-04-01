30d0669bfd4329855d8fbd32f2e08f2e068da69c
trmaphi <trmaphi@gmail.com>
2019-04-01 16:37:03 +0700

    Fix package scope

--- data-structures-java/array/rotation/OnebyOne.java
+++ data-structures-java/array/rotation/OnebyOne.java
@@ -1,8 +1,9 @@
-package rotation;
+package array.rotation;
 
 public class OnebyOne {
     static void leftRotate(int[] arr, int p, int n) {
         for (int i = 0; i < p; i++) {
+            // Rotate one by one
             int j, temp;
             temp = arr[0];
             for (j = 0; j < n - 1; j++) {
@@ -23,7 +24,7 @@ public class OnebyOne {
         int[] arr = {1, 2, 3, 4, 5, 6, 7};
         printArray(arr);
         leftRotate(arr, 2, 7);
-        printArray(arr);
+
     }
 }
 
