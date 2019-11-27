644be21ddd93e647437009292ff6f08a90c76d2d
trmaphi <trmaphi@gmail.com>
2019-11-27 19:10:11 +0700

    lint: style with clang-format

--- dynamic-memory-management-c/aligned_alloc.c
+++ dynamic-memory-management-c/aligned_alloc.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
-    int *p1 = malloc(10 * sizeof *p1);
-    printf("default-aligned addr:   %p\n", (void *)p1);
-    free(p1);
+int main(void) {
+  int *p1 = malloc(10 * sizeof *p1);
+  printf("default-aligned addr:   %p\n", (void *)p1);
+  free(p1);
 
-    int *p2 = aligned_alloc(1024, 1024 * sizeof *p2);
-    printf("1024-byte aligned addr: %p\n", (void *)p2);
-    free(p2);
+  int *p2 = aligned_alloc(1024, 1024 * sizeof *p2);
+  printf("1024-byte aligned addr: %p\n", (void *)p2);
+  free(p2);
 }
