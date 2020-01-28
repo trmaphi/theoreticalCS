55f7f66b387ec3b62e60f382be5340b741b7a0c5
trmaphi <trmaphi@gmail.com>
2020-01-28 17:24:04 +0700

    chore: add comments to vm example

--- virtual-machine-c/main.c
+++ virtual-machine-c/main.c
@@ -6,9 +6,10 @@
 #include <stdbool.h>
 #include <stdio.h>
 
-bool running = true;
-int ip = 0;
-int sp = -1;
+bool running = true; // Indicate the machine still running
+int ip = 0;  // Instruction pointer, to count which instruction is executing.
+             // Must be updated if the next instruction start running
+int sp = -1; //
 
 int stack[256];
 
@@ -16,20 +17,26 @@ typedef enum { PSH, ADD, POP, HLT } InstructionSet;
 
 const int program[] = {PSH, 5, PSH, 6, ADD, POP, HLT};
 
+/**
+ * Get the current instruction by Instruction pointer value
+ **/
 int fetch() { return program[ip]; }
 
 void eval(int instr) {
   switch (instr) {
+  // Give halt signal to stop vm
   case HLT: {
     running = false;
     printf("done\n");
     break;
   }
+  // Push value to stack
   case PSH: {
     sp++;
     stack[sp] = program[++ip];
     break;
   }
+  // Pop value from stack and print out popped value
   case POP: {
     int val_popped = stack[sp--];
     printf("popped %d\n", val_popped);
